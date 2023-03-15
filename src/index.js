ee705d2fc16c61176bf40daae576e00db0b9e72a
zjoney <zdjoey@126.com>
2023-03-15 10:12:27 +0800

    style: 💄 Markup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom/client';
 import FixedSizeList from './fixed-size-list';
 import VariableSizeList from './variable-size-list';
 import DynamicSizeList from './dynamic-size-list';
-// import Virtuoso from './Virtuoso'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<FixedSizeList />);
